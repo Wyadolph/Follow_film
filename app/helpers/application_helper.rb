b6601ab0ba0ed02f1c66cb51db9d1c8fcd0121eb
Wyadolph <wyadolph@gmail.com>
2013-11-14 18:32:34 +0800

    add some features on user message pages

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,6 @@
 module ApplicationHelper
 	def full_title(page_title)
-		base_title="Ruby on Rails Tutorial Sample App"
+		base_title="Follow the film and find your way"
 		if page_title.empty?
 			base_title
 		else
