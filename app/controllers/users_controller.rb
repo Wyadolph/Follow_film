2363d467e5c3c430fbc192a76123f581a8790e7f
Wyadolph <wyadolph@gmail.com>
2013-11-14 20:11:24 +0800

    Add "User sign up" and save user info to database done!

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,6 +3,25 @@ class UsersController < ApplicationController
   def show
       @user=User.find(params[:id])
   end
+
   def new
+  	@user = User.new
   end
+
+  def create
+  	@user = User.new(user_params)
+  	if @user.save  # if save to db succeed, then redirect to user_message page
+  		flash[:success] = "Welcome to the Follow_film App!"
+  		redirect_to @user
+  	else  # if save to db failed, reload signup page
+  		render 'new'
+  	end
+  end
+
+  private
+  def user_params
+  	params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  	
+  end
+
 end
